efaa5f96cf30eb9418b0cd0706ec877cebe50e49
Dane Bulat <dane.bulat@gmail.com>
2023-11-15 07:50:19 +0000

    wdio test updated

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -4,6 +4,6 @@ import assert from 'node:assert';
 describe('Electron Testing', () => {
   it('should return the correct application name', async () => {
     const appName = await browser.electron.app('getName');
-    assert.strictEqual(appName, 'Polkadot Live');
+    assert.strictEqual(appName, 'polkadot-live');
   });
 });
