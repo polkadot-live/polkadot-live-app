bd538b9bb6f8c63918a8bcedf71a53b33fa53315
Dane Bulat <dane.bulat@gmail.com>
2023-11-15 07:19:14 +0000

    mocha unit testing setup

---
+++ test/unit/hello.test.ts
@@ -0,0 +1,7 @@
+import assert from 'node:assert';
+
+describe('Hello', function () {
+  it('should pass the assertion', function () {
+    assert.strictEqual(true, true);
+  });
+});
---
+++ test/unit/world.test.ts
@@ -0,0 +1,7 @@
+import assert from 'node:assert';
+
+describe('World', function () {
+  it('should pass the assertion', function () {
+    assert.strictEqual(true, true);
+  });
+});
