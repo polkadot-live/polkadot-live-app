b6b7ce0b1eaaa08daab0aae34d601e5cb421124a
Ross Bulat <ross@parity.io>
2023-11-18 17:10:01 +0000

    fix: multiple react versions, use `ellipsisFn`.

--- src/renderer/screens/Import/Ledger/index.tsx
+++ src/renderer/screens/Import/Ledger/index.tsx
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 import {
-  clipAddress,
+  ellipsisFn,
   localStorageOrDefault,
   setStateWithRef,
 } from '@polkadot-cloud/utils';
@@ -81,7 +81,7 @@ export const ImportLedger = ({
         ...device,
         pubKey,
         address,
-        name: clipAddress(address),
+        name: ellipsisFn(address),
       }));
 
       const newAddresses = addressesRef.current
