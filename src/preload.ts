30d6de9ec31fb260a8dba2f7350d991b549ba5c7
Ross Bulat <ross@parity.io>
2023-12-24 16:26:46 +0700

    format

--- src/preload.ts
+++ src/preload.ts
@@ -17,21 +17,28 @@ if (isTest) {
 
 contextBridge.exposeInMainWorld('myAPI', {
   // Window lifecycle
+  // ----------------
 
   quitApp: (): void => {
     ipcRenderer.invoke('app:quit');
   },
   hideWindow: (id) => ipcRenderer.send('app:window:hide', id),
+
   closeWindow: (id: string) => ipcRenderer.send('app:window:close', id),
 
   // Chain management
+  // ----------------
 
   syncChain: (callback) => ipcRenderer.on('renderer:chain:sync', callback),
+
   chainAdded: (callback) => ipcRenderer.on('renderer:chain:added', callback),
+
   chainRemoved: (callback) =>
     ipcRenderer.on('renderer:chain:removed', callback),
+
   chainConnected: (callback) =>
     ipcRenderer.on('renderer:chain:connected', callback),
+
   chainDisconnected: (callback) =>
     ipcRenderer.on('renderer:chain:disconnected', callback),
   // NOTE: Not being called in any renderers (main doesn't receive app:chain:remove)
@@ -76,6 +83,7 @@ contextBridge.exposeInMainWorld('myAPI', {
     ipcRenderer.on('renderer:event:dismiss', callback),
 
   // Subscription communication
+  // --------------------------
 
   // Report chain subscriptions to renderer.
   reportChainSubscriptionState: (callback) =>
@@ -90,6 +98,7 @@ contextBridge.exposeInMainWorld('myAPI', {
     ipcRenderer.invoke('app:subscriptions:task:handle', data),
 
   // Transactions
+  // ------------
 
   // Requests to main process to initiate a transaction.
   requestInitTx: (chain, from, nonce, pallet, method, args) =>
@@ -114,6 +123,7 @@ contextBridge.exposeInMainWorld('myAPI', {
     ipcRenderer.send('app:tx:vault:submit', signature),
 
   // Miscellaneous
+  // -------------
 
   // Request to open a URL in the browser.
   openBrowserURL: (url) => ipcRenderer.send('app:url:open', url),
