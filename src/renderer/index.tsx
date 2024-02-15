a5fef24e49067940c9e2b8b3e3b9f152e36dabe7
Ross Bulat <ross@parity.io>
2024-02-15 15:27:58 +0700

    feat(refactor): add local theme fiels (#337)

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,19 +1,15 @@
-// Basic app styles.
-import './index.scss';
+// Copyright 2023 @paritytech/polkadot-live authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import { Providers } from './Providers';
+import { createRoot } from 'react-dom/client';
 
 // Network themes.
 import '@polkadot-cloud/core/accent/polkadot-relay.css';
 
-// Default template fonts.
-import '@polkadot-cloud/core/theme/default/fonts/index.css';
-// Default template theme.
-import '@polkadot-cloud/core/theme/default/index.css';
-
-// Polkadot Cloud core styles.
-import '@polkadot-cloud/core/css/styles/index.css';
-
-import { Providers } from './Providers';
-import { createRoot } from 'react-dom/client';
+// App styles.
+import './theme/theme.css';
+import './theme/index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
