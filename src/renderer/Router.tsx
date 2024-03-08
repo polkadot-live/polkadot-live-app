05f2700f3e0c83c11388461bc72f61d943dd45ed
Dane Bulat <dane.bulat@gmail.com>
2024-03-08 14:06:01 +0000

    renderer initialization + online switching logic moved to useInitIpcHandlers hook

--- src/renderer/Router.tsx
+++ src/renderer/Router.tsx
@@ -4,7 +4,7 @@
 import { MainInterfaceWrapper } from '@app/Wrappers';
 import { Overlay } from '@app/library/Overlay';
 import { Tooltip } from '@app/library/Tooltip';
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import { Action } from '@app/screens/Action';
 import { Home } from '@app/screens/Home';
@@ -12,126 +12,27 @@ import { Import } from '@app/screens/Import';
 import { useAddresses } from '@app/contexts/Addresses';
 import { useOnlineStatus } from './contexts/OnlineStatus';
 import { useManage } from '@app/screens/Home/Manage/provider';
-import { useSubscriptions } from './contexts/Subscriptions';
 import { useTheme } from 'styled-components';
 import type { AnyJson } from '@/types/misc';
 import type { FlattenedAccounts } from '@/types/accounts';
 import type { IpcRendererEvent } from 'electron';
-
-import * as AccountUtils from '@/utils/AccountUtils';
-import { AccountsController } from '@/controller/renderer/AccountsController';
-import { APIsController } from '@/controller/renderer/APIsController';
-import { ChainList } from '@/config/chains';
-import { SubscriptionsController } from '@/controller/renderer/SubscriptionsController';
-import { useChains } from './contexts/Chains';
 import { useMessagePorts } from '@app/hooks/useMessagePorts';
+import { useInitIpcHandlers } from '@app/hooks/useInitIpcHandlers';
 
 export const RouterInner = () => {
   const { mode }: AnyJson = useTheme();
   const { setAddresses } = useAddresses();
 
-  const { setChainSubscriptions, setAccountSubscriptions } = useSubscriptions();
   const { setRenderedSubscriptions } = useManage();
   const { setOnline } = useOnlineStatus();
-  const { addChain } = useChains();
 
   // Set up message ports communication between windows.
   useMessagePorts();
 
-  const refAppInitialized = useRef(false);
+  // Set up app initialization handlers.
+  useInitIpcHandlers();
 
   useEffect(() => {
-    // Handle app initialization.
-    window.myAPI.initializeApp(async () => {
-      if (!refAppInitialized.current) {
-        // Initialize accounts from persisted state.
-        await AccountsController.initialize();
-
-        // Initialize chain APIs.
-        await APIsController.initialize(Array.from(ChainList.keys()));
-
-        // Use API instance to initialize account nomination pool data.
-        if (await window.myAPI.getOnlineStatus()) {
-          await AccountUtils.fetchAccountNominationPoolData();
-        }
-
-        // Initialize persisted account subscriptions.
-        await AccountsController.subscribeAccounts();
-
-        // Initialize persisted chain subscriptions.
-        await SubscriptionsController.initChainSubscriptions();
-
-        // Set accounts to render.
-        setAddresses(AccountsController.accounts);
-
-        // Set chain subscriptions data for rendering.
-        setChainSubscriptions(SubscriptionsController.getChainSubscriptions());
-
-        // Set account subscriptions data for rendering.
-        setAccountSubscriptions(
-          SubscriptionsController.getAccountSubscriptions(
-            AccountsController.accounts
-          )
-        );
-
-        // Report chain connections to UI.
-        for (const apiData of APIsController.getAllFlattenedAPIData()) {
-          addChain(apiData);
-        }
-
-        refAppInitialized.current = true;
-        console.log('App initialized...');
-      }
-      console.log('App already initialized...');
-    });
-
-    // Handle switching to online mode.
-    window.myAPI.initializeAppOffline(async () => {
-      // Unsubscribe queryMulti API calls for managed accounts.
-      AccountsController.unsubscribeAccounts();
-
-      // Unsubscribe queryMulti API calls for managed chains.
-      SubscriptionsController.unsubscribeChains();
-
-      // Disconnect from any active API instances.
-      for (const chainId of Array.from(ChainList.keys())) {
-        await APIsController.close(chainId);
-      }
-
-      // Report online status to renderer.
-      setOnline(await window.myAPI.getOnlineStatus());
-    });
-
-    // Handle switching to online mode.
-    window.myAPI.initializeAppOnline(async () => {
-      // Fetch up-to-date nomination pool data for managed accounts.
-      await AccountUtils.fetchAccountNominationPoolData();
-
-      // Re-subscribe to managed accounts cached subscription tasks.
-      await AccountsController.resubscribeAccounts();
-
-      // Re-subscribe to managed chain subscription tasks.
-      await SubscriptionsController.resubscribeAccounts();
-
-      // Report online status to renderer.
-      setOnline(await window.myAPI.getOnlineStatus());
-
-      // Set chain subscriptions data for rendering.
-      setChainSubscriptions(SubscriptionsController.getChainSubscriptions());
-
-      // Set account subscriptions data for rendering.
-      setAccountSubscriptions(
-        SubscriptionsController.getAccountSubscriptions(
-          AccountsController.accounts
-        )
-      );
-
-      // Report chain connections to UI.
-      for (const apiData of APIsController.getAllFlattenedAPIData()) {
-        addChain(apiData);
-      }
-    });
-
     // Handle initial responses to populate state from store.
     window.myAPI.reportImportedAccounts(
       (_: IpcRendererEvent, accounts: string) => {
@@ -142,7 +43,6 @@ export const RouterInner = () => {
     );
 
     window.myAPI.reportOnlineStatus((_: IpcRendererEvent, status: boolean) => {
-      console.log(`Online status STATE received: ${status}`);
       setOnline(status);
     });
   }, []);
---
+++ src/renderer/hooks/useInitIpcHandlers.ts
@@ -0,0 +1,121 @@
+// Copyright 2024 @rossbulat/polkadot-live-app authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import { AccountsController } from '@/controller/renderer/AccountsController';
+import { APIsController } from '@/controller/renderer/APIsController';
+import { ChainList } from '@/config/chains';
+import { fetchAccountNominationPoolData } from '@/utils/AccountUtils';
+import { SubscriptionsController } from '@/controller/renderer/SubscriptionsController';
+import { useAddresses } from '@app/contexts/Addresses';
+import { useChains } from '@app/contexts/Chains';
+import { useEffect, useRef } from 'react';
+import { useOnlineStatus } from '@app/contexts/OnlineStatus';
+import { useSubscriptions } from '@app/contexts/Subscriptions';
+
+export const useInitIpcHandlers = () => {
+  const { addChain } = useChains();
+  const { setAddresses } = useAddresses();
+  const { setChainSubscriptions, setAccountSubscriptions } = useSubscriptions();
+  const { setOnline } = useOnlineStatus();
+
+  const refAppInitialized = useRef(false);
+
+  useEffect(() => {
+    /**
+     * Handle app initialization.
+     */
+    window.myAPI.initializeApp(async () => {
+      if (!refAppInitialized.current) {
+        // Initialize accounts from persisted state.
+        await AccountsController.initialize();
+
+        // Initialize chain APIs.
+        await APIsController.initialize(Array.from(ChainList.keys()));
+
+        // Use API instance to initialize account nomination pool data.
+        if (await window.myAPI.getOnlineStatus()) {
+          await fetchAccountNominationPoolData();
+        }
+
+        // Initialize persisted account subscriptions.
+        await AccountsController.subscribeAccounts();
+
+        // Initialize persisted chain subscriptions.
+        await SubscriptionsController.initChainSubscriptions();
+
+        // Set accounts to render.
+        setAddresses(AccountsController.accounts);
+
+        // Set chain subscriptions data for rendering.
+        setChainSubscriptions(SubscriptionsController.getChainSubscriptions());
+
+        // Set account subscriptions data for rendering.
+        setAccountSubscriptions(
+          SubscriptionsController.getAccountSubscriptions(
+            AccountsController.accounts
+          )
+        );
+
+        // Report chain connections to UI.
+        for (const apiData of APIsController.getAllFlattenedAPIData()) {
+          addChain(apiData);
+        }
+
+        refAppInitialized.current = true;
+        console.log('App initialized...');
+      }
+      console.log('App already initialized...');
+    });
+
+    /**
+     * Handle switching to offline mode.
+     */
+    window.myAPI.initializeAppOffline(async () => {
+      // Unsubscribe queryMulti API calls for managed accounts.
+      AccountsController.unsubscribeAccounts();
+
+      // Unsubscribe queryMulti API calls for managed chains.
+      SubscriptionsController.unsubscribeChains();
+
+      // Disconnect from any active API instances.
+      for (const chainId of Array.from(ChainList.keys())) {
+        await APIsController.close(chainId);
+      }
+
+      // Report online status to renderer.
+      setOnline(await window.myAPI.getOnlineStatus());
+    });
+
+    /**
+     * Handle switching to online mode.
+     */
+    window.myAPI.initializeAppOnline(async () => {
+      // Fetch up-to-date nomination pool data for managed accounts.
+      await fetchAccountNominationPoolData();
+
+      // Re-subscribe to managed accounts cached subscription tasks.
+      await AccountsController.resubscribeAccounts();
+
+      // Re-subscribe to managed chain subscription tasks.
+      await SubscriptionsController.resubscribeAccounts();
+
+      // Report online status to renderer.
+      setOnline(await window.myAPI.getOnlineStatus());
+
+      // Set chain subscriptions data for rendering.
+      setChainSubscriptions(SubscriptionsController.getChainSubscriptions());
+
+      // Set account subscriptions data for rendering.
+      setAccountSubscriptions(
+        SubscriptionsController.getAccountSubscriptions(
+          AccountsController.accounts
+        )
+      );
+
+      // Report chain connections to UI.
+      for (const apiData of APIsController.getAllFlattenedAPIData()) {
+        addChain(apiData);
+      }
+    });
+  }, []);
+};
