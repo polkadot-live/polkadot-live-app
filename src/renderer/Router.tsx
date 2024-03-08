23014290639ff526c38df0753fcc6c4ca6fe6215
Dane Bulat <dane.bulat@gmail.com>
2024-03-08 13:24:51 +0000

    message port logic moved to useMessagePorts hook

--- src/renderer/Router.tsx
+++ src/renderer/Router.tsx
@@ -14,7 +14,6 @@ import { useOnlineStatus } from './contexts/OnlineStatus';
 import { useManage } from '@app/screens/Home/Manage/provider';
 import { useSubscriptions } from './contexts/Subscriptions';
 import { useTheme } from 'styled-components';
-import { ConfigRenderer } from '@/config/ConfigRenderer';
 import type { AnyJson } from '@/types/misc';
 import type { FlattenedAccounts } from '@/types/accounts';
 import type { IpcRendererEvent } from 'electron';
@@ -24,144 +23,24 @@ import { AccountsController } from '@/controller/renderer/AccountsController';
 import { APIsController } from '@/controller/renderer/APIsController';
 import { ChainList } from '@/config/chains';
 import { SubscriptionsController } from '@/controller/renderer/SubscriptionsController';
-import { fetchNominationPoolDataForAccount } from '@/utils/AccountUtils';
 import { useChains } from './contexts/Chains';
+import { useMessagePorts } from '@app/hooks/useMessagePorts';
 
 export const RouterInner = () => {
   const { mode }: AnyJson = useTheme();
-  const { setAddresses, importAddress, removeAddress } = useAddresses();
+  const { setAddresses } = useAddresses();
 
   const { setChainSubscriptions, setAccountSubscriptions } = useSubscriptions();
   const { setRenderedSubscriptions } = useManage();
   const { setOnline } = useOnlineStatus();
   const { addChain } = useChains();
 
+  // Set up message ports communication between windows.
+  useMessagePorts();
+
   const refAppInitialized = useRef(false);
 
   useEffect(() => {
-    const handlePorts = () => {
-      // Handle port communication.
-      window.onmessage = (e: MessageEvent) => {
-        switch (e.data.target) {
-          case 'main': {
-            // Cache main port on renderer config.
-            ConfigRenderer.portMain = e.ports[0];
-
-            // Receive data from `import` port.
-            ConfigRenderer.portMain.onmessage = async (ev: MessageEvent) => {
-              switch (ev.data.task) {
-                // Import an address received from the `import` window.
-                case 'address:import': {
-                  console.log('> Executing address:import');
-                  const { chainId, source, address, name } = ev.data.data;
-
-                  // Add address to accounts controller.
-                  const account = AccountsController.add(
-                    chainId,
-                    source,
-                    address,
-                    name
-                  );
-
-                  // If account was unsuccessfully added, exit early.
-                  if (!account) {
-                    console.log(
-                      'Account could not be added, probably already added'
-                    );
-                    return;
-                  }
-
-                  // Initialize nomination pool data for account if necessary.
-                  await fetchNominationPoolDataForAccount(account, chainId);
-
-                  // Add account to address context state.
-                  importAddress(chainId, source, address, name);
-
-                  // Update account subscriptions data.
-                  setAccountSubscriptions(
-                    SubscriptionsController.getAccountSubscriptions(
-                      AccountsController.accounts
-                    )
-                  );
-
-                  break;
-                }
-                case 'address:remove': {
-                  console.log('> Executing address:remove');
-                  const { address, chainId } = ev.data.data;
-
-                  // Retrieve the account.
-                  const account = AccountsController.get(chainId, address);
-
-                  if (!account) {
-                    console.log(
-                      'Account could not be added, probably already added'
-                    );
-                    return;
-                  }
-
-                  // Unsubscribe from all active tasks.
-                  await AccountsController.removeAllSubscriptions(account);
-
-                  // Remove account from controller and store.
-                  AccountsController.remove(chainId, address);
-
-                  // Remove address from context.
-                  removeAddress(chainId, address);
-
-                  // Update account subscriptions data.
-                  setAccountSubscriptions(
-                    SubscriptionsController.getAccountSubscriptions(
-                      AccountsController.accounts
-                    )
-                  );
-
-                  // Report chain connections to UI.
-                  for (const apiData of APIsController.getAllFlattenedAPIData()) {
-                    addChain(apiData);
-                  }
-
-                  // Transition away from rendering toggles.
-                  setRenderedSubscriptions({ type: '', tasks: [] });
-
-                  break;
-                }
-                default: {
-                  throw new Error(`Port task not recognized (${ev.data.task})`);
-                }
-              }
-            };
-
-            // Start receiving messages from import port.
-            ConfigRenderer.portMain.start();
-
-            break;
-          }
-          case 'import': {
-            // Cache import port on renderer config.
-            ConfigRenderer.portImport = e.ports[0];
-
-            // Receive data from `main` port.
-            ConfigRenderer.portImport.onmessage = (ev: MessageEvent) => {
-              console.log('> TODO: Data received from main renderer:');
-              console.log(ev.data);
-            };
-
-            // Start receiving messages from main port.
-            ConfigRenderer.portImport.start();
-
-            break;
-          }
-          default: {
-            console.error('Something went wrong.');
-            break;
-          }
-        }
-      };
-    };
-
-    handlePorts();
-
     // Handle app initialization.
     window.myAPI.initializeApp(async () => {
       if (!refAppInitialized.current) {
