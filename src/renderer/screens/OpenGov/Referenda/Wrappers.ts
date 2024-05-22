93990eb45f3481ab9aeacddf8d3a1c12090bb1a7
Dane Bulat <dane.bulat@gmail.com>
2024-05-22 16:04:42 +0700

    referenda control buttons + state

--- src/renderer/screens/OpenGov/Referenda/Wrappers.ts
+++ src/renderer/screens/OpenGov/Referenda/Wrappers.ts
@@ -5,9 +5,62 @@ import styled from 'styled-components';
 
 /**
  * Provides the following styled components:
+ *   ControlsWrapper
  *   ReferendaGroup
  *   ReferendumRowWrapper
  */
+
+export const ControlsWrapper = styled.div`
+  display: flex;
+  column-gap: 1rem;
+  margin-bottom: 1rem;
+
+  .icon-wrapper {
+    opacity: 0.75;
+    display: flex;
+    column-gap: 0.75rem;
+    align-items: center;
+    min-width: 120px;
+
+    position: relative;
+    border: 1px solid #535353;
+    border-radius: 1.25rem;
+
+    margin: 0;
+    padding: 0.3rem 0.5rem;
+    transition: border 0.1s ease-out;
+    user-select: none;
+    cursor: pointer;
+    transition: opacity 0.1s ease-out;
+
+    span {
+      display: inline-block;
+      padding-right: 0.7rem;
+      color: #666666;
+      font-size: 0.9rem;
+    }
+    .icon {
+      color: #5f5f5f;
+      margin-left: 0.7rem;
+    }
+    &:hover {
+      opacity: 0.9;
+    }
+    // Button is active.
+    &.active {
+      border-color: #454545;
+      background-color: #3a3a3a;
+      transition: opacity 0.1s ease-out;
+      .icon,
+      span {
+        color: #ededed;
+      }
+      &:hover {
+        background-color: #3a3a3a;
+      }
+    }
+  }
+`;
 
 export const ReferendaGroup = styled.div`
   --container-border-radius: 1.25rem;
--- src/renderer/screens/OpenGov/Referenda/index.tsx
+++ src/renderer/screens/OpenGov/Referenda/index.tsx
@@ -6,13 +6,23 @@ import { DragClose } from '@/renderer/library/DragClose';
 import type { ReferendaProps } from '../types';
 import { OpenGovFooter, Scrollable } from '../Wrappers';
 import { ButtonPrimaryInvert } from '@/renderer/kits/Buttons/ButtonPrimaryInvert';
-import { faCaretLeft } from '@fortawesome/pro-solid-svg-icons';
+import {
+  faCaretLeft,
+  faLayerGroup,
+  faTimer,
+} from '@fortawesome/pro-solid-svg-icons';
 import { useReferenda } from '@/renderer/contexts/openGov/Referenda';
 import { ReferendumRow } from './ReferendumRow';
-import { ReferendaGroup } from './Wrappers';
+import { ControlsWrapper, ReferendaGroup } from './Wrappers';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
 
 export const Referenda = ({ setSection, chainId }: ReferendaProps) => {
   const { fetchingReferenda, getSortedActiveReferenda } = useReferenda();
+
+  /// Sorting controls state.
+  const [newestFirst, setNewestFirst] = useState(true);
+  const [groupingOn, setGroupingOn] = useState(false);
 
   return (
     <>
@@ -24,15 +34,39 @@ export const Referenda = ({ setSection, chainId }: ReferendaProps) => {
       </HeaderWrapper>
       <Scrollable>
         <ContentWrapper style={{ padding: '1rem 2rem 0' }}>
-          {fetchingReferenda ? (
-            <p>Loading referenda...</p>
-          ) : (
-            <ReferendaGroup>
-              {getSortedActiveReferenda().map((referendum, i) => (
-                <ReferendumRow key={i} referendum={referendum} />
-              ))}
-            </ReferendaGroup>
-          )}
+          {/* Sorting controls */}
+          <ControlsWrapper>
+            <div
+              className={newestFirst ? 'icon-wrapper active' : 'icon-wrapper'}
+              onClick={() => setNewestFirst(!newestFirst)}
+            >
+              <div className="icon">
+                <FontAwesomeIcon icon={faTimer} />
+              </div>
+              <span>{newestFirst ? 'Newest First' : 'Oldest First'}</span>
+            </div>
+            <div
+              className={groupingOn ? 'icon-wrapper active' : 'icon-wrapper'}
+              onClick={() => setGroupingOn(!groupingOn)}
+            >
+              <div className="icon">
+                <FontAwesomeIcon icon={faLayerGroup} />
+              </div>
+              <span>{groupingOn ? 'Grouping On' : 'Grouping Off'}</span>
+            </div>
+          </ControlsWrapper>
+          {/* List referenda */}
+          <section>
+            {fetchingReferenda ? (
+              <p>Loading referenda...</p>
+            ) : (
+              <ReferendaGroup>
+                {getSortedActiveReferenda().map((referendum, i) => (
+                  <ReferendumRow key={i} referendum={referendum} />
+                ))}
+              </ReferendaGroup>
+            )}
+          </section>
         </ContentWrapper>
       </Scrollable>
       <OpenGovFooter $chainId={chainId}>
