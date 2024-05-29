7e0d2d0bd05c952020da56b06c0a14331153d8cc
Dane Bulat <dane.bulat@gmail.com>
2024-05-29 13:35:24 +0700

    refactored event listing accordion headers + wide option

--- src/renderer/library/Accordion/AccordionCaretHeaders.tsx
+++ src/renderer/library/Accordion/AccordionCaretHeaders.tsx
@@ -5,10 +5,12 @@ import { AccordionHeader, useAccordion } from '@/renderer/library/Accordion';
 import { HeadingWrapper } from '@/renderer/screens/Home/Manage/Wrappers';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown, faCaretRight } from '@fortawesome/pro-solid-svg-icons';
+import { HeadingWrapper as WideHeadingWrapper } from '@/renderer/screens/Home/Wrappers';
 
 interface AccordionCaretHeaderProps {
   title: string;
   itemIndex: number;
+  wide?: boolean;
 }
 
 interface AccordionCaretSwitchHeaderProps {
@@ -20,29 +22,38 @@ interface AccordionCaretSwitchHeaderProps {
 export const AccordionCaretHeader = ({
   title,
   itemIndex,
+  wide,
 }: AccordionCaretHeaderProps) => {
   const { activeIndex } = useAccordion();
 
-  return (
-    <HeadingWrapper>
-      <AccordionHeader>
-        <div className="flex">
-          <div className="left">
-            <div className="icon-wrapper">
-              {activeIndex.includes(itemIndex) ? (
-                <FontAwesomeIcon icon={faCaretDown} transform={'shrink-1'} />
-              ) : (
-                <FontAwesomeIcon icon={faCaretRight} transform={'shrink-1'} />
-              )}
-            </div>
-            <h5>
-              <span>{title}</span>
-            </h5>
+  const innerMarkup = () => (
+    <AccordionHeader>
+      <div className="flex">
+        <div className="left">
+          <div className="icon-wrapper">
+            {activeIndex.includes(itemIndex) ? (
+              <FontAwesomeIcon icon={faCaretDown} transform={'shrink-1'} />
+            ) : (
+              <FontAwesomeIcon icon={faCaretRight} transform={'shrink-1'} />
+            )}
           </div>
+          <h5>
+            <span>{title}</span>
+          </h5>
         </div>
-      </AccordionHeader>
-    </HeadingWrapper>
+      </div>
+    </AccordionHeader>
   );
+
+  if (wide) {
+    return (
+      <WideHeadingWrapper style={{ padding: '0' }}>
+        {innerMarkup()}
+      </WideHeadingWrapper>
+    );
+  } else {
+    return <HeadingWrapper>{innerMarkup()}</HeadingWrapper>;
+  }
 };
 
 export const AccordionCaretSwitchHeader = ({
--- src/renderer/screens/Home/Events/Category.tsx
+++ src/renderer/screens/Home/Events/Category.tsx
@@ -1,28 +1,18 @@
 // Copyright 2024 @rossbulat/polkadot-live-app authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-import {
-  AccordionHeader,
-  AccordionItem,
-  AccordionPanel,
-} from '@/renderer/library/Accordion';
-import { HeadingWrapper } from '../Wrappers';
+import { AccordionItem, AccordionPanel } from '@/renderer/library/Accordion';
 import { EventGroup } from './Wrappers';
 import { getEventChainId } from '@/utils/EventUtils';
-import type { EventCategoryProps } from './types';
-import {
-  faBlock,
-  faCaretDown,
-  faCaretRight,
-} from '@fortawesome/pro-solid-svg-icons';
+import { faBlock } from '@fortawesome/pro-solid-svg-icons';
 import { getCategory } from '@/config/chains';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { EventItem } from './EventItem';
+import { AccordionCaretHeader } from '@/renderer/library/Accordion/AccordionCaretHeaders';
+import type { EventCategoryProps } from './types';
 
 export const Category = ({
   category,
   events,
-  accordionActiveIndices,
   accordionIndex,
 }: EventCategoryProps) => {
   /// Get category's readable name.
@@ -32,28 +22,11 @@ export const Category = ({
   return (
     <EventGroup>
       <AccordionItem>
-        <HeadingWrapper>
-          <AccordionHeader>
-            <div className="flex">
-              <div className="left">
-                <div className="icon-wrapper">
-                  {accordionActiveIndices.includes(accordionIndex) ? (
-                    <FontAwesomeIcon
-                      icon={faCaretDown}
-                      transform={'shrink-1'}
-                    />
-                  ) : (
-                    <FontAwesomeIcon
-                      icon={faCaretRight}
-                      transform={'shrink-1'}
-                    />
-                  )}
-                </div>
-                <h5>{getCategoryName(category)}</h5>
-              </div>
-            </div>
-          </AccordionHeader>
-        </HeadingWrapper>
+        <AccordionCaretHeader
+          title={getCategoryName(category)}
+          itemIndex={accordionIndex}
+          wide={true}
+        />
         <AccordionPanel>
           <div className="items-wrapper">
             {events?.map((event) => (
