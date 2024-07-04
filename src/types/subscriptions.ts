2177919e4d4ffa5089e1c76dbeafc59ebca29083
Dane Bulat <dane.bulat@gmail.com>
2024-07-04 19:08:35 +0700

    add spendable balance subscription config

--- src/types/subscriptions.ts
+++ src/types/subscriptions.ts
@@ -75,6 +75,7 @@ export type TaskAction =
   | 'subscribe:account:balance:free'
   | 'subscribe:account:balance:frozen'
   | 'subscribe:account:balance:reserved'
+  | 'subscribe:account:balance:spendable'
   | 'subscribe:account:nominationPools:rewards'
   | 'subscribe:account:nominationPools:state'
   | 'subscribe:account:nominationPools:renamed'
