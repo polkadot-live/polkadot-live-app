1de109425578872c70b3edd8f80437bdd572a0e3
Dane Bulat <dane.bulat@gmail.com>
2024-08-12 23:01:35 +0700

    remove local storage keys from import window config

--- src/config/processes/import.ts
+++ src/config/processes/import.ts
@@ -1,8 +1,6 @@
 // Copyright 2024 @polkadot-live/polkadot-live-app authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-import type { AccountSource } from '@/types/accounts';
-
 /**
  * @name Config
  * @summary Configuration class for the `import` window. Accessed in the import renderer.
@@ -10,29 +8,6 @@ import type { AccountSource } from '@/types/accounts';
 export class Config {
   // Cache the import window's message port to to facilitate communication to the `main` renderer.
   private static _portImport: MessagePort;
-
-  // Local storage keys.
-  private static _ledgerAddressesStorageKey = 'ledger_addresses';
-  private static _vaultAddressesStorageKey = 'vault_addresses';
-  private static _readOnlyAddressesStorageKey = 'read_only_addresses';
-
-  // Return the local storage key for corresponding source addresses.
-  static getStorageKey(source: AccountSource): string {
-    switch (source) {
-      case 'ledger': {
-        return Config._ledgerAddressesStorageKey;
-      }
-      case 'vault': {
-        return Config._vaultAddressesStorageKey;
-      }
-      case 'read-only': {
-        return Config._readOnlyAddressesStorageKey;
-      }
-      default: {
-        throw new Error('source not recognized');
-      }
-    }
-  }
 
   // Get the `import` window's message port.
   static get portImport(): MessagePort {
