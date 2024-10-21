1e41b5da91c02106e8185d842c10397132d14fb9
Dane Bulat <dane.bulat@gmail.com>
2024-10-21 18:44:46 +0700

    add copyright comment

--- src/renderer/library/components/SortControls/types.ts
+++ src/renderer/library/components/SortControls/types.ts
@@ -1,3 +1,6 @@
+// Copyright 2024 @polkadot-live/polkadot-live-app authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
 import type { AnyFunction } from '@/types/misc';
 import type { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 
