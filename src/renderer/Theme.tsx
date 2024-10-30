96b198bbdface6d00bd34d18dd218fc6e9e353c5
Dane Bulat <dane.bulat@gmail.com>
2024-10-30 15:34:14 +0700

    feat: enable dynamic theme class

--- src/renderer/Theme.tsx
+++ src/renderer/Theme.tsx
@@ -2,27 +2,27 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 //import { setStateWithRef } from '@w3ux/utils';
+import { setStateWithRef } from '@w3ux/utils';
 import { Router } from './Router';
 import { useRef, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 export const Theme = () => {
   // Check whether system is initially on dark mode.
-  //const { matches: isDarkMode } = window.matchMedia(
-  //  '(prefers-color-scheme: dark)'
-  //);
+  const { matches: isDarkMode } = window.matchMedia(
+    '(prefers-color-scheme: dark)'
+  );
 
   // Store initial theme in state.
-  //const [mode, setMode] = useState(isDarkMode ? 'dark' : 'light');
-  const [mode] = useState('dark');
+  const [mode, setMode] = useState(isDarkMode ? 'dark' : 'light');
   const modeRef = useRef(mode);
 
   // Subscribe to system theme changes.
-  //window
-  //  .matchMedia('(prefers-color-scheme: dark)')
-  //  .addEventListener('change', (event) => {
-  //    setStateWithRef(event.matches ? 'dark' : 'light', setMode, modeRef);
-  //  });
+  window
+    .matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', (event) => {
+      setStateWithRef(event.matches ? 'dark' : 'light', setMode, modeRef);
+    });
 
   return (
     <ThemeProvider theme={{ mode: modeRef.current }}>
