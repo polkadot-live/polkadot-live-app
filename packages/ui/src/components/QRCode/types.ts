1dfa101f60495c3e43e7dd2ec04e8760b3dcb1d3
Dane Bulat <dane.bulat@gmail.com>
2024-11-15 15:09:02 +0700

    update polkdaot packages

--- packages/ui/src/components/QRCode/types.ts
+++ packages/ui/src/components/QRCode/types.ts
@@ -1,8 +1,8 @@
 // Copyright 2024 @polkadot-live/polkadot-live-app authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-import type { HexString } from '@polkadot/util/types';
 import type React from 'react';
+import type { HexString } from '@polkadot/util/types';
 import type { Html5Qrcode } from 'html5-qrcode';
 
 export interface Html5QrScannerProps {
