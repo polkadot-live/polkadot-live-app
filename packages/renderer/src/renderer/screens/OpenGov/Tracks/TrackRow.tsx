264a67dde063155d439fb56a1999b2137bf51761
Dane Bulat <dane.bulat@gmail.com>
2024-11-15 14:44:26 +0700

    fix circular dependency - move util function to renderer package

--- packages/renderer/src/renderer/screens/OpenGov/Tracks/TrackRow.tsx
+++ packages/renderer/src/renderer/screens/OpenGov/Tracks/TrackRow.tsx
@@ -14,7 +14,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { formatBlocksToTime } from '@ren/renderer/utils/timeUtils';
 import { useHelp } from '@ren/renderer/contexts/common/Help';
 import { useTracks } from '@ren/renderer/contexts/openGov/Tracks';
-import { formatChainUnits } from '@polkadot-live/ui/utils';
+import { formatChainUnits } from '@app/utils/renderingUtils';
 import type { HelpItemKey } from '@polkadot-live/types/help';
 import type { TrackRowProps } from '../types';
 
