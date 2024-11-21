5b9e0b19240e9bc0b71d2c97c545f9e5ab5509ee
Dane Bulat <dane.bulat@gmail.com>
2024-11-22 00:21:41 +0700

    feat: preload test suite (#749)

---
+++ packages/preload/tests/unit.spec.ts
@@ -0,0 +1,22 @@
+// Copyright 2024 @polkadot-live/polkadot-live-app authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import { API, KEY_CONFIG } from '../src/preload';
+import { contextBridge } from 'electron';
+
+vi.mock('electron', () => ({
+  contextBridge: {
+    exposeInMainWorld: vi.fn(),
+  },
+  ipcRenderer: {
+    on: vi.fn(),
+  },
+}));
+
+describe('Preload API', () => {
+  it('should all be exposed', () => {
+    const { exposeInMainWorld } = vi.mocked(contextBridge);
+    expect(exposeInMainWorld).toHaveBeenCalledWith(KEY_CONFIG.apiKey, API);
+    expect(exposeInMainWorld).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ packages/preload/vitest.config.ts
@@ -0,0 +1,16 @@
+// Copyright 2024 @polkadot-live/polkadot-live-app authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    testTimeout: 5_000,
+    hookTimeout: 5_000,
+
+    /**
+     * Import vitest functions in every test file.
+     */
+    globals: true,
+  },
+});
