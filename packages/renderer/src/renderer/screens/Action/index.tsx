985b969b2af99632608cb590d3423f04851c2d42
Dane Bulat <dane.bulat@gmail.com>
2025-01-23 18:19:47 +0700

    extrinsics window footer

--- packages/renderer/src/renderer/screens/Action/index.tsx
+++ packages/renderer/src/renderer/screens/Action/index.tsx
@@ -11,7 +11,7 @@ import { useTxMeta } from '@app/contexts/action/TxMeta';
 import { useActionMessagePorts } from '@app/hooks/useActionMessagePorts';
 import { useDebug } from '@app/hooks/useDebug';
 import { ComponentFactory } from './TxActionItem';
-import { Scrollable } from '@polkadot-live/ui/styles';
+import { Scrollable, StatsFooter } from '@polkadot-live/ui/styles';
 import { AccordionContent, AccordionTrigger } from './Accordion';
 import { AccordionWrapper } from './Accordion/Wrappers';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -90,112 +90,120 @@ export const Action = () => {
   };
 
   return (
-    <Scrollable
-      $footerHeight={0}
-      $headerHeight={0}
-      style={{ paddingTop: 0, paddingBottom: 20 }}
-    >
-      <div style={{ padding: '0.5rem 1rem', height: '90%' }}>
-        {Array.from(extrinsics.keys()).length === 0 && (
-          <EmptyExtrinsicsWrapper>
-            <div>
-              <FontAwesomeIcon
-                icon={faSpinner}
-                style={{ fontSize: '7rem', opacity: '0.6' }}
-              />
-              <p>No extrinsics have been added yet.</p>
-            </div>
-          </EmptyExtrinsicsWrapper>
-        )}
+    <>
+      <Scrollable $headerHeight={0}>
+        <div style={{ padding: '0.5rem 1rem 2rem' }}>
+          {Array.from(extrinsics.keys()).length === 0 && (
+            <EmptyExtrinsicsWrapper>
+              <div>
+                <FontAwesomeIcon
+                  icon={faSpinner}
+                  style={{ fontSize: '7rem', opacity: '0.6' }}
+                />
+                <p>No extrinsics have been added yet.</p>
+              </div>
+            </EmptyExtrinsicsWrapper>
+          )}
 
-        {Array.from(extrinsics.keys()).length !== 0 && (
-          <AccordionWrapper>
-            <Accordion.Root
-              className="AccordionRoot"
-              type="multiple"
-              defaultValue={[Array.from(extrinsics.keys())[0]]}
-            >
-              {Array.from(extrinsics.entries()).map(([txUid, info]) => (
-                <Accordion.Item
-                  key={txUid}
-                  className="AccordionItem"
-                  value={txUid}
-                >
-                  <div
-                    style={{ display: 'flex', gap: '2px', marginTop: '10px' }}
+          {Array.from(extrinsics.keys()).length !== 0 && (
+            <AccordionWrapper>
+              <Accordion.Root
+                className="AccordionRoot"
+                type="multiple"
+                defaultValue={[Array.from(extrinsics.keys())[0]]}
+              >
+                {Array.from(extrinsics.entries()).map(([txUid, info]) => (
+                  <Accordion.Item
+                    key={txUid}
+                    className="AccordionItem"
+                    value={txUid}
                   >
-                    <AccordionTrigger>
-                      <ChevronDownIcon
-                        className="AccordionChevron"
-                        aria-hidden
-                      />
-                      {ComponentFactory[info.actionMeta.action].title}
-                      <span className="right">
-                        <div className="stat">
-                          <FontAwesomeIcon
-                            icon={faObjectGroup}
-                            transform={'shrink-2'}
-                          />
-                          {getCategoryTitle(info)}
-                        </div>
-                        <div className="stat">
-                          <FontAwesomeIcon
-                            icon={faCircleDot}
-                            transform={'shrink-2'}
-                          />
-                          {getTxStatusTitle(info.txStatus)}
-                        </div>
-                      </span>
-                    </AccordionTrigger>
-                    <div className="HeaderContentDropdownWrapper">
-                      <ExtrinsicDropdownMenu
-                        isBuilt={info.dynamicInfo !== undefined}
-                        onBuild={() => initTxDynamicInfo(txUid)}
-                        onDelete={() => removeExtrinsic(txUid)}
-                        onSign={() =>
-                          openOverlayWith(
-                            <SignOverlay
-                              txId={txUid}
+                    <div
+                      style={{ display: 'flex', gap: '2px', marginTop: '10px' }}
+                    >
+                      <AccordionTrigger>
+                        <ChevronDownIcon
+                          className="AccordionChevron"
+                          aria-hidden
+                        />
+                        {ComponentFactory[info.actionMeta.action].title}
+                        <span className="right">
+                          <div className="stat">
+                            <FontAwesomeIcon
+                              icon={faObjectGroup}
+                              transform={'shrink-2'}
+                            />
+                            {getCategoryTitle(info)}
+                          </div>
+                          <div className="stat">
+                            <FontAwesomeIcon
+                              icon={faCircleDot}
+                              transform={'shrink-2'}
+                            />
+                            {getTxStatusTitle(info.txStatus)}
+                          </div>
+                        </span>
+                      </AccordionTrigger>
+                      <div className="HeaderContentDropdownWrapper">
+                        <ExtrinsicDropdownMenu
+                          isBuilt={info.dynamicInfo !== undefined}
+                          onBuild={() => initTxDynamicInfo(txUid)}
+                          onDelete={() => removeExtrinsic(txUid)}
+                          onSign={() =>
+                            openOverlayWith(
+                              <SignOverlay
+                                txId={txUid}
+                                from={info.actionMeta.from}
+                              />,
+                              'small',
+                              true
+                            )
+                          }
+                        />
+                      </div>
+                    </div>
+                    <AccordionContent>
+                      <div>
+                        {ComponentFactory[info.actionMeta.action].description}
+                        <Tx
+                          label={'Signer'}
+                          name={ellipsisFn(info.actionMeta.from)}
+                          notEnoughFunds={false}
+                          dangerMessage={'Danger message'}
+                          estimatedFee={
+                            info.dynamicInfo === undefined
+                              ? '-'
+                              : `${info.dynamicInfo?.estimatedFee} ${chainCurrency(info.actionMeta.chainId)}`
+                          }
+                          SignerComponent={
+                            <Signer
+                              txId={info.txId}
+                              txBuilt={info.dynamicInfo !== undefined}
+                              submitting={info.submitting}
+                              valid={!info.submitting}
                               from={info.actionMeta.from}
-                            />,
-                            'small',
-                            true
-                          )
-                        }
-                      />
-                    </div>
-                  </div>
-                  <AccordionContent>
-                    <div>
-                      {ComponentFactory[info.actionMeta.action].description}
-                      <Tx
-                        label={'Signer'}
-                        name={ellipsisFn(info.actionMeta.from)}
-                        notEnoughFunds={false}
-                        dangerMessage={'Danger message'}
-                        estimatedFee={
-                          info.dynamicInfo === undefined
-                            ? '-'
-                            : `${info.dynamicInfo?.estimatedFee} ${chainCurrency(info.actionMeta.chainId)}`
-                        }
-                        SignerComponent={
-                          <Signer
-                            txId={info.txId}
-                            txBuilt={info.dynamicInfo !== undefined}
-                            submitting={info.submitting}
-                            valid={!info.submitting}
-                            from={info.actionMeta.from}
-                          />
-                        }
-                      />
-                    </div>
-                  </AccordionContent>
-                </Accordion.Item>
-              ))}
-            </Accordion.Root>
-          </AccordionWrapper>
-        )}
-      </div>
-    </Scrollable>
+                            />
+                          }
+                        />
+                      </div>
+                    </AccordionContent>
+                  </Accordion.Item>
+                ))}
+              </Accordion.Root>
+            </AccordionWrapper>
+          )}
+        </div>
+      </Scrollable>
+      <StatsFooter $chainId={'Polkadot'}>
+        <div>
+          <section className="left">
+            <div className="footer-stat">
+              <h2>Total Extrinsics:</h2>
+              <span>{Array.from(extrinsics.keys()).length}</span>
+            </div>
+          </section>
+        </div>
+      </StatsFooter>
+    </>
   );
 };
