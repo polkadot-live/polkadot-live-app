08204c278bcd3d960dc2f6864742d561f66d2aec
Dane Bulat <dane.bulat@gmail.com>
2025-02-01 14:11:48 +0700

    move components to wrappers and types files

--- packages/renderer/src/renderer/screens/Home/Send/index.tsx
+++ packages/renderer/src/renderer/screens/Home/Send/index.tsx
@@ -16,13 +16,10 @@ import { useState } from 'react';
 import { ellipsisFn } from '@w3ux/utils';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight, faCopy } from '@fortawesome/free-solid-svg-icons';
-import styled from 'styled-components';
+import { AddButton, InputWrapper } from './Wrappers';
+import type { ActionItemSendProps, SelectBoxProps } from './types';
 
 /** Action item component */
-interface ActionItemSendProps {
-  text: string;
-}
-
 const ActionItemSend = ({ text }: ActionItemSendProps) => (
   <ActionItem
     text={text}
@@ -34,14 +31,6 @@ const ActionItemSend = ({ text }: ActionItemSendProps) => (
 );
 
 /** Select box wrapper */
-interface SelectBoxProps {
-  children: React.ReactNode;
-  ariaLabel: string;
-  placeholder: string;
-  value: string;
-  onValueChange: (val: string) => void;
-}
-
 const SelectBox = ({
   children,
   ariaLabel,
@@ -245,58 +234,6 @@ export const Send: React.FC = () => {
     </div>
   );
 };
-
-const AddButton = styled.button`
-  background-color: var(--button-background-primary);
-  margin-top: 0.4rem;
-
-  display: flex;
-  gap: 0.75rem;
-  justify-content: center;
-  align-items: center;
-  color: var(--text-bright);
-  padding: 1rem 1.5rem;
-  border-radius: 0.375rem;
-  transition: all 0.2s ease-out;
-  user-select: none;
-
-  &:hover:not(:disabled) {
-    filter: brightness(1.2);
-  }
-  &:disabled {
-    opacity: 0.7;
-    cursor: not-allowed;
-  }
-`;
-
-const InputWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 1.25rem;
-  background-color: var(--background-primary);
-  border-radius: 0.375rem;
-
-  input {
-    padding: 0;
-    color: var(--text-color-primary);
-    font-size: 1.2rem;
-    font-weight: 500;
-    width: 100%;
-    text-align: left;
-  }
-  span {
-    color: var(--text-color-primary);
-    font-size: 1.25rem;
-    margin-left: 5px;
-  }
-
-  input[type='number']::-webkit-inner-spin-button,
-  input[type='number']::-webkit-outer-spin-button {
-    -webkit-appearance: none;
-    appearance: none;
-    margin: 0;
-  }
-`;
 
 const FlexColumn = ({ children }: { children: React.ReactNode }) => (
   <section style={{ display: 'flex', flexDirection: 'column', gap: '2px' }}>
---
+++ packages/renderer/src/renderer/screens/Home/Send/types.ts
@@ -0,0 +1,14 @@
+// Copyright 2024 @polkadot-live/polkadot-live-app authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+export interface ActionItemSendProps {
+  text: string;
+}
+
+export interface SelectBoxProps {
+  children: React.ReactNode;
+  ariaLabel: string;
+  placeholder: string;
+  value: string;
+  onValueChange: (val: string) => void;
+}
