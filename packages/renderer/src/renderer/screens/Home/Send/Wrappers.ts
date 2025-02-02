bf661884ffbb65ed7f14850602829492e0408e53
Dane Bulat <dane.bulat@gmail.com>
2025-02-02 18:09:53 +0700

    feat: dynamic send screen (#839)

--- packages/renderer/src/renderer/screens/Home/Send/Wrappers.ts
+++ packages/renderer/src/renderer/screens/Home/Send/Wrappers.ts
@@ -17,6 +17,12 @@ export const InputWrapper = styled.div`
     font-weight: 500;
     width: 100%;
     text-align: left;
+    cursor: default;
+
+    &:disabled {
+      opacity: 0.4;
+      cursor: not-allowed;
+    }
   }
   span {
     color: var(--text-color-primary);
@@ -66,5 +72,29 @@ export const ProgressBarWrapper = styled.div`
     background-color: var(--accent-primary);
     height: 100%;
     transition: width 0.2s ease-in-out;
+  }
+`;
+
+export const NextStepArrowWrapper = styled.div<{ $complete: boolean }>`
+  margin-top: 0.75rem;
+  > button {
+    svg {
+      background-color: ${(props) =>
+        props.$complete ? '#417041' : 'var(--text-color-secondary)'};
+      width: 22px;
+      height: 22px;
+      border-radius: 100%;
+    }
+    text-align: center;
+    width: 100%;
+    color: var(--background-surface);
+    opacity: ${(props) => (props.$complete ? '1' : '0.15')};
+    transition: all 0.2s ease-out;
+    cursor: ${(props) => (props.$complete ? 'pointer' : 'not-allowed')};
+
+    &:hover {
+      filter: ${(props) =>
+        props.$complete ? 'brightness(140%)' : 'brightness(100%)'};
+    }
   }
 `;
--- packages/renderer/src/renderer/screens/Home/Send/index.tsx
+++ packages/renderer/src/renderer/screens/Home/Send/index.tsx
@@ -5,11 +5,14 @@ import * as Accordion from '@radix-ui/react-accordion';
 import * as Select from '@radix-ui/react-select';
 import * as UI from '@polkadot-live/ui/components';
 import * as themeVariables from '../../../theme/variables';
+
+import { chainCurrency } from '@ren/config/chains';
 import { Identicon, MainHeading } from '@polkadot-live/ui/components';
 import { useConnections } from '@app/contexts/common/Connections';
 import { ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { ellipsisFn } from '@w3ux/utils';
+import { getAddressChainId } from '@app/Utils';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faCaretDown,
@@ -17,9 +20,21 @@ import {
   faCircleCheck,
   faCopy,
 } from '@fortawesome/free-solid-svg-icons';
-import { AddButton, InputWrapper, ProgressBarWrapper } from './Wrappers';
-import type { SelectBoxProps } from './types';
-import styled from 'styled-components';
+import {
+  AddButton,
+  InputWrapper,
+  NextStepArrowWrapper,
+  ProgressBarWrapper,
+} from './Wrappers';
+
+import type {
+  AccountSource,
+  LedgerLocalAddress,
+  LocalAddress,
+} from '@polkadot-live/types/accounts';
+import type { ChainID } from '@polkadot-live/types/chains';
+import type { ChangeEvent } from 'react';
+import type { SelectBoxProps, SendAccordionValue } from './types';
 
 /** Progress bar component */
 const ProgressBar = ({ value, max }: { value: number; max: number }) => {
@@ -70,34 +85,219 @@ const SelectBox = ({
 
 /** Send component. */
 export const Send: React.FC = () => {
-  const [sender, setSender] = useState('');
-  const [receiver, setReceiver] = useState('');
+  /**
+   * Addresses fetched from main process.
+   */
+  const [addressMap, setAddressMap] = useState(
+    new Map<AccountSource, (LocalAddress | LedgerLocalAddress)[]>()
+  );
+  const addressMapRef = useRef<typeof addressMap>(addressMap);
+  const [updateCache, setUpdateCache] = useState(false);
+  const [progress, setProgress] = useState(0);
+
+  const [sender, setSender] = useState<null | string>(null);
+  const [receiver, setReceiver] = useState<null | string>(null);
+  const [senderNetwork, setSenderNetwork] = useState<ChainID | null>(null);
+  const [sendAmount, setSendAmount] = useState<string>('0');
+
+  /**
+   * Accordion state.
+   */
+  const [accordionValue, setAccordionValue] = useState<SendAccordionValue[]>([
+    'section-sender',
+  ]);
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [progress, setProgress] = useState(20);
+  const [estimatedFee, setEstimatedFee] = useState<string>('');
 
-  const mockAddresses = [
-    {
-      accountName: 'Mock Account 1',
-      address: 'MockAddress1',
-    },
-    {
-      accountName: 'Mock Account 2',
-      address: 'MockAddress2',
-    },
-    {
-      accountName: 'Mock Account 3',
-      address: 'MockAddress3',
-    },
-    {
-      accountName: 'Mock Account 4',
-      address: 'MockAddress4',
-    },
-    {
-      accountName: 'Mock Account 5',
-      address: 'MockAddress5',
-    },
-  ];
+  /**
+   * Fetch stored addresss from main when component loads.
+   */
+  useEffect(() => {
+    const fetch = async () => {
+      const serialized = (await window.myAPI.rawAccountTask({
+        action: 'raw-account:getAll',
+        data: null,
+      })) as string;
+
+      const parsedMap = new Map<AccountSource, string>(JSON.parse(serialized));
+
+      for (const [source, ser] of parsedMap.entries()) {
+        switch (source) {
+          case 'vault':
+          case 'read-only':
+          case 'wallet-connect': {
+            const parsed: LocalAddress[] = JSON.parse(ser);
+            addressMapRef.current.set(source, parsed);
+            setUpdateCache(true);
+            break;
+          }
+          case 'ledger': {
+            const parsed: LedgerLocalAddress[] = JSON.parse(ser);
+            addressMapRef.current.set(source, parsed);
+            setUpdateCache(true);
+            break;
+          }
+          default: {
+            continue;
+          }
+        }
+      }
+    };
+
+    fetch();
+  }, []);
+
+  /**
+   * Mechanism for updating address map state from an async process.
+   */
+  useEffect(() => {
+    if (updateCache) {
+      setAddressMap(addressMapRef.current);
+      setUpdateCache(false);
+    }
+  }, [updateCache]);
+
+  /**
+   * Progress bar controller.
+   */
+  useEffect(() => {
+    let conditions = 0;
+
+    sender && (conditions += 1);
+    receiver && (conditions += 1);
+    sendAmount !== '0' && sendAmount !== '' && (conditions += 1);
+
+    switch (conditions) {
+      case 1: {
+        setProgress(100 / 3);
+        break;
+      }
+      case 2: {
+        setProgress((100 / 3) * 2);
+        break;
+      }
+      case 3: {
+        setProgress(100);
+        break;
+      }
+      default: {
+        setProgress(0);
+        break;
+      }
+    }
+  }, [sender, receiver, sendAmount]);
+
+  /**
+   * Sender value changed callback.
+   */
+  const handleSenderChange = (senderAddress: string) => {
+    setSender(senderAddress);
+    setSenderNetwork(getAddressChainId(senderAddress));
+
+    // Reset other send fields.
+    setReceiver(null);
+    setSendAmount('0');
+  };
+
+  /**
+   * Send amount changed callback.
+   */
+  const handleSendAmountChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const val = event.target.value;
+    if (val === '' || !isNaN(Number(val))) {
+      setSendAmount(val === '' ? '' : val);
+    }
+  };
+
+  /**
+   * Remove zero when send amount field focused.
+   */
+  const handleSendAmountFocus = () => {
+    if (sendAmount === '0') {
+      setSendAmount('');
+    }
+  };
+
+  /**
+   * Add zero when send amount field blurred and empty.
+   */
+  const handleSendAmountBlur = () => {
+    if (sendAmount === '') {
+      setSendAmount('0');
+    }
+  };
+
+  /**
+   * Return all addresses capable of signing extrinsics.
+   */
+  const getSenderAccounts = () => {
+    const targetSources: AccountSource[] = ['vault'];
+    let result: LocalAddress[] = [];
+
+    for (const source of targetSources) {
+      const addresses = addressMap.get(source);
+      if (!addresses || addresses.length === 0) {
+        continue;
+      }
+      result = result.concat(addresses as LocalAddress[]);
+    }
+    return result.sort((a, b) => a.name.localeCompare(b.name));
+  };
+
+  /**
+   * Return all addresses for receiver address list.
+   */
+  const getReceiverAccounts = () => {
+    let result: (LocalAddress | LedgerLocalAddress)[] = [];
+    for (const addresses of addressMap.values()) {
+      result = result.concat(addresses);
+    }
+
+    // Filter accounts on sender address network.
+    return (
+      result
+        .filter(({ address }) => {
+          if (!senderNetwork) {
+            return true;
+          } else {
+            return getAddressChainId(address) === senderNetwork;
+          }
+        })
+        // Don't include sender in list.
+        .filter(({ address }) => address !== sender)
+        .sort((a, b) => a.name.localeCompare(b.name))
+    );
+  };
+
+  /**
+   * Conditions to enable the proceed button.
+   */
+  const proceedDisabled = () =>
+    sender === null ||
+    receiver === null ||
+    sendAmount === '0' ||
+    sendAmount === '';
+
+  /**
+   * Handle clicking a green next step arrow.
+   */
+  const handleNextStep = (completed: SendAccordionValue) => {
+    switch (completed) {
+      case 'section-sender': {
+        setAccordionValue(['section-receiver']);
+        break;
+      }
+      case 'section-receiver': {
+        setAccordionValue(['section-send-amount']);
+        break;
+      }
+      case 'section-send-amount': {
+        setAccordionValue(['section-summary']);
+        break;
+      }
+    }
+  };
 
   return (
     <div
@@ -120,22 +320,25 @@ export const Send: React.FC = () => {
           className="AccordionRoot"
           style={{ display: 'flex', flexDirection: 'column', gap: '1.25rem' }}
           type="multiple"
-          defaultValue={['section-sender']}
+          value={accordionValue}
+          onValueChange={(val) =>
+            setAccordionValue(val as SendAccordionValue[])
+          }
         >
           {/** Sender Section */}
           <Accordion.Item className="AccordionItem" value="section-sender">
             <UI.AccordionTrigger narrow={true}>
-              <TriggerContent label="Sender" complete={true} />
+              <TriggerContent label="Sender" complete={sender !== null} />
             </UI.AccordionTrigger>
             <UI.AccordionContent narrow={true}>
               <FlexColumn>
                 <SelectBox
-                  value={sender}
+                  value={sender || ''}
                   ariaLabel="Sender"
                   placeholder="Select Sender"
-                  onValueChange={(val) => setSender(val)}
+                  onValueChange={(val) => handleSenderChange(val)}
                 >
-                  {mockAddresses.map(({ accountName, address }) => (
+                  {getSenderAccounts().map(({ name: accountName, address }) => (
                     <UI.SelectItem key={`sender-${address}`} value={address}>
                       <div className="innerRow">
                         <div>
@@ -156,7 +359,7 @@ export const Send: React.FC = () => {
                         alignItems: 'center',
                       }}
                     >
-                      {sender === '' ? (
+                      {!sender ? (
                         <span>-</span>
                       ) : (
                         <>
@@ -171,7 +374,10 @@ export const Send: React.FC = () => {
                     </div>
                   }
                 />
-                <NextStepArrow complete={true} />
+                <NextStepArrow
+                  complete={sender !== null}
+                  onClick={() => handleNextStep('section-sender')}
+                />
               </FlexColumn>
             </UI.AccordionContent>
           </Accordion.Item>
@@ -179,26 +385,31 @@ export const Send: React.FC = () => {
           {/** Receiver Section */}
           <Accordion.Item className="AccordionItem" value="section-receiver">
             <UI.AccordionTrigger narrow={true}>
-              <TriggerContent label="Receiver" complete={false} />
+              <TriggerContent label="Receiver" complete={receiver !== null} />
             </UI.AccordionTrigger>
             <UI.AccordionContent narrow={true}>
               <FlexColumn>
                 <SelectBox
-                  value={receiver}
+                  value={receiver || ''}
                   ariaLabel="Receiver"
                   placeholder="Select Receiver"
                   onValueChange={(val) => setReceiver(val)}
                 >
-                  {mockAddresses.map(({ accountName, address }) => (
-                    <UI.SelectItem key={`receiver-${address}`} value={address}>
-                      <div className="innerRow">
-                        <div>
-                          <Identicon value={address} size={20} />
+                  {getReceiverAccounts().map(
+                    ({ name: accountName, address }) => (
+                      <UI.SelectItem
+                        key={`receiver-${address}`}
+                        value={address}
+                      >
+                        <div className="innerRow">
+                          <div>
+                            <Identicon value={address} size={20} />
+                          </div>
+                          <div>{accountName}</div>
                         </div>
-                        <div>{accountName}</div>
-                      </div>
-                    </UI.SelectItem>
-                  ))}
+                      </UI.SelectItem>
+                    )
+                  )}
                 </SelectBox>
                 <InfoPanel
                   label={'Receiving Address:'}
@@ -210,7 +421,7 @@ export const Send: React.FC = () => {
                         alignItems: 'center',
                       }}
                     >
-                      {receiver === '' ? (
+                      {!receiver ? (
                         <span>-</span>
                       ) : (
                         <>
@@ -224,7 +435,10 @@ export const Send: React.FC = () => {
                     </div>
                   }
                 />
-                <NextStepArrow complete={false} />
+                <NextStepArrow
+                  complete={receiver !== null}
+                  onClick={() => handleNextStep('section-receiver')}
+                />
               </FlexColumn>
             </UI.AccordionContent>
           </Accordion.Item>
@@ -232,21 +446,30 @@ export const Send: React.FC = () => {
           {/** Send Amount Section */}
           <Accordion.Item className="AccordionItem" value="section-send-amount">
             <UI.AccordionTrigger narrow={true}>
-              <TriggerContent label="Send Amount" complete={false} />
+              <TriggerContent
+                label="Send Amount"
+                complete={sendAmount !== '0' && sendAmount !== ''}
+              />
             </UI.AccordionTrigger>
             <UI.AccordionContent narrow={true}>
               <FlexColumn>
                 <InputWrapper>
                   <input
                     type="number"
-                    disabled={false}
-                    defaultValue={0}
-                    onChange={(e) => console.log(e)}
+                    disabled={!sender}
+                    value={sendAmount}
+                    defaultValue={'0'}
+                    onChange={(e) => handleSendAmountChange(e)}
+                    onFocus={() => handleSendAmountFocus()}
+                    onBlur={() => handleSendAmountBlur()}
                   />
                   <span>DOT</span>
                 </InputWrapper>
                 <InfoPanel label={'Spendable Balance:'} Content={'100 DOT'} />
-                <NextStepArrow complete={false} />
+                <NextStepArrow
+                  complete={!(sendAmount === '0' || sendAmount === '')}
+                  onClick={() => handleNextStep('section-send-amount')}
+                />
               </FlexColumn>
             </UI.AccordionContent>
           </Accordion.Item>
@@ -258,12 +481,35 @@ export const Send: React.FC = () => {
             </UI.AccordionTrigger>
             <UI.AccordionContent narrow={true}>
               <FlexColumn>
-                <InfoPanel label={'Network:'} Content={'Polkadot'} />
-                <InfoPanel label={'Sender:'} Content={'Mock Account 1'} />
-                <InfoPanel label={'Receiver:'} Content={'Mock Account 2'} />
-                <InfoPanel label={'Send Amount:'} Content={'10 DOT'} />
-                <InfoPanel label={'Estimated Fee:'} Content={'0.1 DOT'} />
-                <AddButton onClick={() => console.log('Add')} disabled={false}>
+                <InfoPanel label={'Network:'} Content={senderNetwork || '-'} />
+                <InfoPanel
+                  label={'Sender:'}
+                  Content={!sender ? '-' : ellipsisFn(sender!, 12)}
+                />
+                <InfoPanel
+                  label={'Receiver:'}
+                  Content={!receiver ? '-' : ellipsisFn(receiver, 12)}
+                />
+                <InfoPanel
+                  label={'Send Amount:'}
+                  Content={
+                    sendAmount === '0'
+                      ? '-'
+                      : `${sendAmount} ${chainCurrency(senderNetwork!)}`
+                  }
+                />
+                <InfoPanel
+                  label={'Estimated Fee:'}
+                  Content={
+                    estimatedFee === ''
+                      ? '-'
+                      : `${estimatedFee} ${chainCurrency(senderNetwork!)}`
+                  }
+                />
+                <AddButton
+                  onClick={() => console.log('Add')}
+                  disabled={proceedDisabled()}
+                >
                   <FontAwesomeIcon
                     icon={faChevronRight}
                     transform={'shrink-4'}
@@ -343,30 +589,16 @@ const TriggerContent = ({
   </>
 );
 
-const NextStepArrowWrapper = styled.div<{ $complete: boolean }>`
-  > button {
-    text-align: center;
-    width: 100%;
-    font-size: 2rem;
-    color: ${(props) =>
-      props.$complete
-        ? 'var(--accent-success)'
-        : 'var(--text-color-secondary)'};
-    opacity: ${(props) => (props.$complete ? '1' : '0.15')};
-    transition: all 0.2s ease-out;
-    cursor: ${(props) => (props.$complete ? 'pointer' : 'not-allowed')};
-
-    &:hover {
-      filter: ${(props) =>
-        props.$complete ? 'brightness(140%)' : 'brightness(100%)'};
-    }
-  }
-`;
-
-const NextStepArrow = ({ complete }: { complete: boolean }) => (
+const NextStepArrow = ({
+  complete,
+  onClick,
+}: {
+  complete: boolean;
+  onClick: () => void;
+}) => (
   <NextStepArrowWrapper $complete={complete}>
-    <button>
-      <FontAwesomeIcon icon={faCaretDown} />
+    <button disabled={!complete} onClick={() => onClick()}>
+      <FontAwesomeIcon icon={faCaretDown} transform={'shrink-6'} />
     </button>
   </NextStepArrowWrapper>
 );
