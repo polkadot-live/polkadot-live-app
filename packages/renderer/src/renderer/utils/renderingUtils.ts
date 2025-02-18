36dacec2afb73a480a2eef20ec97c41d25ac776c
Dane Bulat <dane.bulat@gmail.com>
2025-02-18 15:18:57 +0700

    subscription row - radix tooltips + rendering functions

--- packages/renderer/src/renderer/utils/renderingUtils.ts
+++ packages/renderer/src/renderer/utils/renderingUtils.ts
@@ -34,25 +34,6 @@ export const showGroupTooltip = (task: SubscriptionTask) => {
   }
 };
 
-export const getTooltipClassForGroup = (task: SubscriptionTask) => {
-  const { account, category } = task;
-  if (!account) {
-    return '';
-  }
-
-  switch (category) {
-    case 'Nomination Pools': {
-      return account.nominationPoolData ? '' : 'tooltip-trigger-element';
-    }
-    case 'Nominating': {
-      return account.nominatingData ? '' : 'tooltip-trigger-element';
-    }
-    default: {
-      return '';
-    }
-  }
-};
-
 /**
  * @name toolTipTextFor
  * @summary Utility for fetching tooltip text for a subscription switch.
@@ -61,10 +42,10 @@ export const getTooltipClassForGroup = (task: SubscriptionTask) => {
 export const toolTipTextFor = (category: TaskCategory) => {
   switch (category) {
     case 'Nominating': {
-      return 'Not nominating';
+      return 'Not Nominating';
     }
     case 'Nomination Pools': {
-      return 'Not in nomination pool';
+      return 'Not In Nomination Pool';
     }
     default: {
       return '';
