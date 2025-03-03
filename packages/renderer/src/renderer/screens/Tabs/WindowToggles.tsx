080f695352dac36e286164e67d552a4797f741ca
Dane Bulat <dane.bulat@gmail.com>
2025-03-03 17:02:27 +0700

    blockout resize toggle group

---
+++ packages/renderer/src/renderer/screens/Tabs/WindowToggles.tsx
@@ -0,0 +1,82 @@
+// Copyright 2024 @polkadot-live/polkadot-live-app authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import * as ToggleGroup from '@radix-ui/react-toggle-group';
+import { DimensionsIcon } from '@radix-ui/react-icons';
+import styled from 'styled-components';
+
+export const ToggleGroupRoot = styled(ToggleGroup.Root)`
+  --border-radius: 0.375rem;
+
+  height: 100%;
+  display: inline-flex;
+  align-items: center;
+  padding-left: 1rem;
+
+  .ToggleGroupItem {
+    background-color: var(--button-background-primary);
+    color: var(--text-color-secondary);
+    margin-top: -4px;
+    display: flex;
+    align-items: center;
+    min-width: 34px;
+    height: 30px;
+    justify-content: center;
+    margin-left: 1px;
+    user-select: none;
+  }
+  .ToggleGroupItem:first-child {
+    margin-left: 0;
+    border-top-left-radius: var(--border-radius);
+    border-bottom-left-radius: var(--border-radius);
+  }
+  .ToggleGroupItem:last-child {
+    border-top-right-radius: var(--border-radius);
+    border-bottom-right-radius: var(--border-radius);
+  }
+  .ToggleGroupItem:hover {
+    background-color: var(--button-background-primary-hover);
+  }
+  .ToggleGroupItem[data-state='on'] {
+    background-color: var(--button-background-primary-hover);
+    color: var(--text-bright);
+  }
+  .ToggleGroupItem:focus {
+    position: relative;
+  }
+`;
+
+export const ResizeToggles = () => {
+  console.log('todo');
+
+  return (
+    <ToggleGroupRoot
+      className="ToggleGroup"
+      type="single"
+      defaultValue="center"
+      aria-label="Window Size"
+    >
+      <ToggleGroup.Item
+        className="ToggleGroupItem"
+        value="left"
+        aria-label="Small window size"
+      >
+        <DimensionsIcon width={'10px'} />
+      </ToggleGroup.Item>
+      <ToggleGroup.Item
+        className="ToggleGroupItem"
+        value="center"
+        aria-label="Medium window size"
+      >
+        <DimensionsIcon width={'12px'} />
+      </ToggleGroup.Item>
+      <ToggleGroup.Item
+        className="ToggleGroupItem"
+        value="right"
+        aria-label="Large window size"
+      >
+        <DimensionsIcon width={'14px'} />
+      </ToggleGroup.Item>
+    </ToggleGroupRoot>
+  );
+};
