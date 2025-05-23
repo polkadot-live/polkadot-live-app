99539498a0c15f5fbc5f4bc927c77d0cf57bc459
Dane Bulat <dane.bulat@gmail.com>
2025-05-23 19:33:01 +0700

    chore: update copyright year (#985)

--- packages/main/src/controller/BackupController.ts
+++ packages/main/src/controller/BackupController.ts
@@ -1,4 +1,4 @@
-// Copyright 2024 @polkadot-live/polkadot-live-app authors & contributors
+// Copyright 2025 @polkadot-live/polkadot-live-app authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
 import { SharedState } from '@/config/SharedState';
