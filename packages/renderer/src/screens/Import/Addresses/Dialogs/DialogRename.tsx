a7e94b9c09184f55790ac174322267c700437cf9
Dane Bulat <dane.bulat@gmail.com>
2025-06-19 20:48:59 +0700

    rename dialog - blockout encoded accounts ui

--- packages/renderer/src/screens/Import/Addresses/Dialogs/DialogRename.tsx
+++ packages/renderer/src/screens/Import/Addresses/Dialogs/DialogRename.tsx
@@ -16,8 +16,14 @@ import { useState } from 'react';
 import { renderToast, validateAccountName } from '@polkadot-live/ui/utils';
 import { TooltipRx } from '@polkadot-live/ui/components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleRight, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCheck,
+  faPenToSquare,
+  faXmark,
+} from '@fortawesome/free-solid-svg-icons';
 import { unescape } from '@w3ux/utils';
+import type { AnyData } from '@polkadot-live/types/misc';
+import type { ChainID } from '@polkadot-live/types/chains';
 import type { FormEvent } from 'react';
 import type { ImportedGenericAccount } from '@polkadot-live/types/accounts';
 
@@ -25,14 +31,42 @@ interface DialogRenameProps {
   genericAccount: ImportedGenericAccount;
 }
 
+const FormLabel = ({
+  htmlFor,
+  text,
+  theme,
+}: {
+  htmlFor: string;
+  text: string;
+  theme: AnyData;
+}) => (
+  <label
+    className="Dialog__Label"
+    style={{
+      color: theme.textColorSecondary,
+      fontSize: '1rem',
+      textAlign: 'left',
+      paddingLeft: '0.25rem',
+    }}
+    htmlFor={htmlFor}
+  >
+    {text}
+  </label>
+);
+
 export const DialogRename = ({ genericAccount }: DialogRenameProps) => {
-  const { accountName, publicKeyHex, source } = genericAccount;
+  const { accountName, encodedAccounts, publicKeyHex, source } = genericAccount;
   const { handleAddressImport, isUniqueAccountName } = useAddresses();
   const { getTheme } = useConnections();
 
   const [dialogOpen, setDialogOpen] = useState<boolean>(false);
   const [inputVal, setInputVal] = useState<string>(accountName);
   const theme = getTheme();
+
+  const nameData: [ChainID, string][] = Object.values(encodedAccounts).map(
+    ({ alias, chainId }) => [chainId, alias]
+  );
+  const [encodedNames, setEncodedNames] = useState(new Map([...nameData]));
 
   /**
    * Cancel button clicked for edit input.
@@ -53,6 +87,7 @@ export const DialogRename = ({ genericAccount }: DialogRenameProps) => {
     genericAccount.accountName = newName;
     handleAddressImport(genericAccount);
   };
+
   /**
    * Validate input and rename account.
    */
@@ -107,11 +142,39 @@ export const DialogRename = ({ genericAccount }: DialogRenameProps) => {
     }
   };
 
+  /**
+   * Rename an encoded account.
+   */
+  const commitEncodedRename = (chainId: ChainID) => {
+    console.log(`>> TODO: ${chainId}`);
+  };
+
+  /**
+   * Handle encoded change.
+   */
+  const handleEncodedChange = (
+    e: FormEvent<HTMLInputElement>,
+    chainId: ChainID
+  ) => {
+    let val = e.currentTarget.value || '';
+    val = unescape(val);
+    setEncodedNames((prev) => new Map(prev).set(chainId, val));
+  };
+
+  /**
+   * Set encoded account to current name.
+   */
+  const resetEncodedEditing = (chainId: ChainID) => {
+    setEncodedNames((prev) =>
+      new Map(prev).set(chainId, encodedAccounts[chainId].alias)
+    );
+  };
+
   return (
     <Dialog.Root open={dialogOpen} onOpenChange={handleOpenChange}>
       <DialogTrigger $theme={theme}>
         <FlexRow $gap={'0.75rem'} className="btn-text">
-          <TooltipRx theme={theme} text={'Rename Account'}>
+          <TooltipRx theme={theme} text={'Rename Accounts'}>
             <FontAwesomeIcon icon={faPenToSquare} transform={'shrink-2'} />
           </TooltipRx>
         </FlexRow>
@@ -126,11 +189,8 @@ export const DialogRename = ({ genericAccount }: DialogRenameProps) => {
           <FlexColumn $rowGap={'1.5rem'}>
             <FlexColumn $rowGap={'0.75rem'}>
               <Dialog.Title className="Dialog__Title">
-                Rename Account
+                Rename Accounts
               </Dialog.Title>
-              <Dialog.Description className="Dialog__Description">
-                Enter a new name for this account.
-              </Dialog.Description>
             </FlexColumn>
 
             <form
@@ -139,38 +199,123 @@ export const DialogRename = ({ genericAccount }: DialogRenameProps) => {
                 commitEdit();
               }}
             >
-              <FlexColumn>
-                <FlexRow className="Dialog__FieldSet">
-                  <label className="Dialog__Label" htmlFor="refId">
-                    <FontAwesomeIcon icon={faAngleRight} />
-                  </label>
-                  <input
-                    value={inputVal}
-                    onChange={handleChange}
-                    className="Dialog__Input"
-                    id="refId"
-                    placeholder="Account Name"
+              <FlexColumn style={{ marginTop: '0.75rem' }}>
+                <FlexColumn $rowGap={'0.75rem'}>
+                  <FormLabel
+                    htmlFor="refId"
+                    text="Primary Name"
+                    theme={theme}
                   />
-                </FlexRow>
-                <FlexRow style={{ justifyContent: 'flex-end' }}>
-                  <button
-                    type="button"
-                    className="Dialog__Button"
-                    disabled={accountName === inputVal}
-                    onClick={() => cancelEditing()}
-                  >
-                    Reset
-                  </button>
-                  <button
-                    type="submit"
-                    className="Dialog__Button"
-                    disabled={accountName === inputVal}
-                  >
-                    Rename
-                  </button>
-                </FlexRow>
+                  <FlexRow>
+                    <FlexRow
+                      className="Dialog__FieldSet"
+                      style={{ padding: '0.25rem 1.25rem' }}
+                    >
+                      <input
+                        value={inputVal}
+                        onChange={handleChange}
+                        className="Dialog__Input"
+                        id="refId"
+                        placeholder="Account Name"
+                      />
+                    </FlexRow>
+                    <FlexRow
+                      $gap={'0.5rem'}
+                      style={{ justifyContent: 'flex-end' }}
+                    >
+                      <TooltipRx text={'Reset'} theme={theme}>
+                        <button
+                          type="button"
+                          className="Dialog__Button"
+                          disabled={accountName === inputVal}
+                          onClick={() => cancelEditing()}
+                        >
+                          <FontAwesomeIcon icon={faXmark} />
+                        </button>
+                      </TooltipRx>
+                      <TooltipRx text={'Apply'} theme={theme}>
+                        <button
+                          type="submit"
+                          className="Dialog__Button"
+                          disabled={accountName === inputVal}
+                        >
+                          <FontAwesomeIcon icon={faCheck} />
+                        </button>
+                      </TooltipRx>
+                    </FlexRow>
+                  </FlexRow>
+                </FlexColumn>
               </FlexColumn>
             </form>
+
+            <span
+              style={{
+                marginTop: '0.6rem',
+                borderBottom: `1px solid ${theme.textDimmed}`,
+                opacity: '0.25',
+              }}
+            ></span>
+
+            {/* Encoded Accounts */}
+            {Array.from(Object.values(encodedAccounts)).map(
+              ({ address, alias, chainId }) => (
+                <form
+                  key={address}
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    commitEncodedRename(chainId);
+                  }}
+                >
+                  <FlexColumn>
+                    <FlexColumn $rowGap={'0.75rem'}>
+                      <FormLabel
+                        htmlFor={`input-${chainId}`}
+                        text={chainId}
+                        theme={theme}
+                      />
+                      <FlexRow>
+                        <FlexRow
+                          className="Dialog__FieldSet"
+                          style={{ padding: '0.25rem 1.25rem' }}
+                        >
+                          <input
+                            value={encodedNames.get(chainId) || ''}
+                            onChange={(e) => handleEncodedChange(e, chainId)}
+                            className="Dialog__Input"
+                            id={`input-${chainId}`}
+                            placeholder="Account Name"
+                          />
+                        </FlexRow>
+                        <FlexRow
+                          $gap={'0.5rem'}
+                          style={{ justifyContent: 'flex-end' }}
+                        >
+                          <TooltipRx text={'Reset'} theme={theme}>
+                            <button
+                              type="button"
+                              className="Dialog__Button"
+                              disabled={encodedNames.get(chainId) === alias}
+                              onClick={() => resetEncodedEditing(chainId)}
+                            >
+                              <FontAwesomeIcon icon={faXmark} />
+                            </button>
+                          </TooltipRx>
+                          <TooltipRx text={'Apply'} theme={theme}>
+                            <button
+                              type="submit"
+                              className="Dialog__Button"
+                              disabled={encodedNames.get(chainId) === alias}
+                            >
+                              <FontAwesomeIcon icon={faCheck} />
+                            </button>
+                          </TooltipRx>
+                        </FlexRow>
+                      </FlexRow>
+                    </FlexColumn>
+                  </FlexColumn>
+                </form>
+              )
+            )}
           </FlexColumn>
         </DialogContent>
       </Dialog.Portal>
