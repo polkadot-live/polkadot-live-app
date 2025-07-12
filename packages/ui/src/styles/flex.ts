424f0cb9379186da6faf0ecdde062b050df36fe8
Dane Bulat <dane.bulat@gmail.com>
2025-07-12 14:19:19 +0700

    no accounts imported ui - use wrapper component

--- packages/ui/src/styles/flex.ts
+++ packages/ui/src/styles/flex.ts
@@ -7,6 +7,7 @@ export const EmptyWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  background-color: transparent !important;
 
   > div {
     display: flex;
