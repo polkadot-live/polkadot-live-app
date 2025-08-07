7974755326c65a1798f9deeb6d1ec98684164305
Dane Bulat <dane.bulat@gmail.com>
2025-08-07 14:52:04 +0700

    ui component: initial offline banner

---
+++ packages/ui/src/components/OfflineBanner/index.tsx
@@ -0,0 +1,22 @@
+// Copyright 2025 @polkadot-live/polkadot-live-app authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import { FlexRow } from '../../styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
+
+const OfflineBannerWrapper = styled(FlexRow)`
+  background-color: #e7ad65;
+  color: #101010;
+  font-size: 1.02rem;
+  justify-content: center;
+  padding: 0.95rem 1rem;
+`;
+
+export const OfflineBanner = () => (
+  <OfflineBannerWrapper $gap={'0.75rem'}>
+    <FontAwesomeIcon icon={faCircleExclamation} transform={'grow-1'} />
+    <span>Currently offline.</span>
+  </OfflineBannerWrapper>
+);
