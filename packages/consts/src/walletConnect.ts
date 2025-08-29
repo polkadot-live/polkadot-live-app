669661f3c19db1c9b14b713b282482593c2a6e5f
Dane Bulat <dane.bulat@gmail.com>
2025-08-29 13:29:46 +0700

    chore(core): paseo asset hub migration (#1073)

--- packages/consts/src/walletConnect.ts
+++ packages/consts/src/walletConnect.ts
@@ -17,7 +17,7 @@ export const WC_POLKADOT_CAIP_ID = '91b171bb158e2d3848fa23a9f1c25182';
 export const WC_KUSAMA_CAIP_ID = 'b0a8d493285c2df73290dfb7e61f870f';
 export const WC_WESTEND_CAIP_ID = 'e143f23803ac50e8f6f8e62695d1ce9e';
 export const WC_WESTMINT_CAIP_ID = '67f9723393ef76214df0118c34bbbd3d';
-export const WC_PASEO_CAIP_ID = '77afd6190f1554ad45fd0d31aee62aac';
+export const WC_PASEO_ASSET_HUB_CAIP_ID = 'd6eec26135305a8ad257a20d00335728';
 
 export const WcNetworks: WcSelectNetwork[] = [
   {
@@ -31,8 +31,8 @@ export const WcNetworks: WcSelectNetwork[] = [
     selected: false,
   },
   {
-    caipId: WC_PASEO_CAIP_ID,
-    chainId: 'Paseo Relay',
+    caipId: WC_PASEO_ASSET_HUB_CAIP_ID,
+    chainId: 'Paseo Asset Hub',
     selected: false,
   },
   {
@@ -45,7 +45,7 @@ export const WcNetworks: WcSelectNetwork[] = [
 export const WcCaipToChainID: Record<string, ChainID> = {
   [WC_POLKADOT_CAIP_ID]: 'Polkadot Relay',
   [WC_KUSAMA_CAIP_ID]: 'Kusama Relay',
-  [WC_PASEO_CAIP_ID]: 'Paseo Relay',
+  [WC_PASEO_ASSET_HUB_CAIP_ID]: 'Paseo Asset Hub',
   [WC_WESTEND_CAIP_ID]: 'Westend Relay',
   [WC_WESTMINT_CAIP_ID]: 'Westend Asset Hub',
 };
@@ -56,8 +56,8 @@ export const getWalletConnectChainId = (chainId: ChainID) => {
       return WC_POLKADOT_CAIP_ID;
     case 'Kusama Relay':
       return WC_KUSAMA_CAIP_ID;
-    case 'Paseo Relay':
-      return WC_PASEO_CAIP_ID;
+    case 'Paseo Asset Hub':
+      return WC_PASEO_ASSET_HUB_CAIP_ID;
     case 'Westend Relay':
       return WC_WESTEND_CAIP_ID;
     case 'Westend Asset Hub':
