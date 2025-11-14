5c496825c1cab50c6fd49cb6511381fe3a9bfa23
Dane Bulat <dane.bulat@gmail.com>
2025-11-14 13:45:35 +0700

    chore(tracks): cleanup

--- packages/ui/src/utils/helpers.tsx
+++ packages/ui/src/utils/helpers.tsx
@@ -1,29 +1,9 @@
 // Copyright 2025 @polkadot-live/polkadot-live-app authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-import { LoadingPlaceholderWrapper } from '@polkadot-live/styles/wrappers';
-
 /**
  * @name createArrayWithLength
  * @summary Utility for creating an array of `n` length.
  */
 export const createArrayWithLength = (n: number): number[] =>
   [...Array(n + 1)].map((_, i) => i);
-
-/**
- * @name renderPlaceholders
- * @summary Render placeholder loaders.
- */
-export const renderPlaceholders = (
-  length: number,
-  height = '3rem',
-  borderRadius = '1.25rem'
-) => (
-  <LoadingPlaceholderWrapper $height={height} $borderRadius={borderRadius}>
-    <div className="placeholder-content-wrapper">
-      {createArrayWithLength(length).map((_, i) => (
-        <div key={i} className="placeholder-content"></div>
-      ))}
-    </div>
-  </LoadingPlaceholderWrapper>
-);
