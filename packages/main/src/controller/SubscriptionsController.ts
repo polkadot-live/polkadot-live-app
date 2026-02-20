f43333ae5962c826bccd630b5cdd030ea69993ed
Dane Bulat <dane.bulat@gmail.com>
2026-02-20 23:32:52 +0700

    refactor(controller): cleanup main subscriptions controller

--- packages/main/src/controller/SubscriptionsController.ts
+++ packages/main/src/controller/SubscriptionsController.ts
@@ -11,20 +11,7 @@ import type { ChainID } from '@polkadot-live/types/chains';
 import type { IpcTask } from '@polkadot-live/types/communication';
 import type { SubscriptionTask } from '@polkadot-live/types/subscriptions';
 
-/**
- * @name SubscriptionsController
- * @summary Manages subscription task persistence.
- *
- * Database tables:
- * `account_subscriptions` table for per-account, per-chain tasks
- * `chain_subscriptions` table for global chain-level tasks
- */
-
 export class SubscriptionsController {
-  /**
-   * @name process
-   * @summary Process a subscription IPC task.
-   */
   static process(task: IpcTask): string | undefined {
     switch (task.action) {
       // Get an account's persisted tasks in serialized form.
@@ -35,8 +22,8 @@ export class SubscriptionsController {
       }
       // Get persisted chain subscription tasks.
       case 'subscriptions:chain:getAll': {
-        const ser = SubscriptionsController.getChainTasks();
-        return ser === '[]' ? '' : ser;
+        const serializedTasks = SubscriptionsController.getChainTasks();
+        return serializedTasks !== '[]' ? serializedTasks : '';
       }
       // Update a persisted account subscription task.
       case 'subscriptions:account:update': {
@@ -60,95 +47,20 @@ export class SubscriptionsController {
     }
   }
 
-  /**
-   * @name get
-   * @summary Get serialized subscriptions from database for an address.
-   */
-  private static get(address: string, chainId: ChainID): string {
-    return AccountSubscriptionsRepository.getForAddress(chainId, address);
-  }
+  // ===== Public =====
 
-  /**
-   * @name getChainTasks
-   * @summary Return serialized chain tasks from database.
-   */
-  private static getChainTasks(): string {
-    return ChainSubscriptionsRepository.getAll();
-  }
-
-  /**
-   * @name doImport
-   * @summary Persist new tasks to database and return them to renderer to process.
-   * Receives serialized tasks from an exported backup file.
-   */
-  private static doImport(ipcTask: IpcTask) {
-    const { serialized }: { serialized: string } = ipcTask.data;
-    const s_array: [string, string][] = JSON.parse(serialized);
-    const s_map = new Map<string, string>(s_array);
-
-    // Iterate map of serialized tasks keyed by an account address.
-    for (const [key, serTasks] of s_map.entries()) {
-      // Clear persisted tasks for an account.
-      const [chainId, address] = key.split(':', 2);
-      SubscriptionsController.clearAccountTasksInStore(
-        address,
-        chainId as ChainID,
-      );
-
-      // Persist backed up tasks to database if online.
-      if (OnlineStatusController.getStatus()) {
-        const received: SubscriptionTask[] = JSON.parse(serTasks);
-        received.forEach((t) => {
-          SubscriptionsController.update(t, address, t.chainId);
-        });
-      }
-    }
-  }
-
-  /**
-   * @name getBackupData
-   * @summary Return a serialized map of account subscription tasks for backup.
-   */
-  static getBackupData(): string {
-    return AccountSubscriptionsRepository.getAllForBackup();
-  }
-
-  /**
-   * @name update
-   * @summary Update a persisted account task with the received data.
-   */
-  private static update(
-    task: SubscriptionTask,
-    address: string,
-    chainId: ChainID,
-  ) {
-    SubscriptionsController.updateTask(task, address, chainId);
-  }
-
-  /**
-   * @name updateChainTask
-   * @summary Update a persisted chain task with the received data.
-   */
-  private static updateChainTask(task: SubscriptionTask) {
-    if (task.status === 'enable') {
-      ChainSubscriptionsRepository.set(task.chainId, task.action, task);
-    } else {
-      ChainSubscriptionsRepository.delete(task.chainId, task.action);
-    }
-  }
-
-  /**
-   * @name clearAccountTasksInStore
-   * @summary Clears an account's persisted subscriptions in the database. Invoked when an account is removed.
-   */
+  // Clears an account's persisted subscriptions in the database.
+  // Invoked when an account is removed.
   static clearAccountTasksInStore(address: string, chainId: ChainID) {
     AccountSubscriptionsRepository.clearForAddress(chainId, address);
   }
 
-  /**
-   * @name updateCachedAccountNameForTasks
-   * @summary Called when an account is renamed.
-   */
+  // Return a serialized map of account subscription tasks for backup.
+  static getBackupData(): string {
+    return AccountSubscriptionsRepository.getAllForBackup();
+  }
+
+  // Called when an account is renamed.
   static updateCachedAccountNameForTasks(
     address: string,
     chainId: ChainID,
@@ -175,23 +87,65 @@ export class SubscriptionsController {
     }
   }
 
-  /*------------------------------------------------------------
-   * Utilities
-   *------------------------------------------------------------*/
+  // ===== Private =====
 
-  private static updateTask(
+  // Get serialized subscriptions for an address.
+  private static get(address: string, chainId: ChainID): string {
+    return AccountSubscriptionsRepository.getForAddress(chainId, address);
+  }
+
+  // Return all serialized chain tasks.
+  private static getChainTasks(): string {
+    return ChainSubscriptionsRepository.getAll();
+  }
+
+  // Persist and return new tasks.
+  private static doImport(ipcTask: IpcTask) {
+    // Receive serialized tasks from backup file.
+    const { serialized }: { serialized: string } = ipcTask.data;
+    const s_array: [string, string][] = JSON.parse(serialized);
+    const s_map = new Map<string, string>(s_array);
+
+    for (const [key, serTasks] of s_map.entries()) {
+      // Clear account tasks.
+      const [chainId, address] = key.split(':', 2);
+      SubscriptionsController.clearAccountTasksInStore(
+        address,
+        chainId as ChainID,
+      );
+      // Persist tasks if online.
+      if (OnlineStatusController.getStatus()) {
+        const received: SubscriptionTask[] = JSON.parse(serTasks);
+        received.forEach((t) => {
+          SubscriptionsController.update(t, address, t.chainId);
+        });
+      }
+    }
+  }
+
+  // Update a persisted account task.
+  private static update(
     task: SubscriptionTask,
     address: string,
     chainId: ChainID,
   ) {
     if (task.status === 'enable') {
-      // Insert or replace the task. SQLite's INSERT OR REPLACE handles upsert automatically.
+      // Insert or replace the task.
       const params = { chainId, address, action: task.action, task };
       AccountSubscriptionsRepository.set(params);
     } else {
       // Otherwise, remove the task.
       const params = { chainId, address, action: task.action };
       AccountSubscriptionsRepository.delete(params);
+    }
+  }
+
+  // Update a persisted chain task.
+  private static updateChainTask(task: SubscriptionTask) {
+    if (task.status === 'enable') {
+      ChainSubscriptionsRepository.set(task.chainId, task.action, task);
+    } else {
+      ChainSubscriptionsRepository.delete(task.chainId, task.action);
     }
   }
 }
