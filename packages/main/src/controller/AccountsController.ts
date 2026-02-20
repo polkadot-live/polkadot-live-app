ca5ef5986a85fd46aa908c4a156b4ff0ac06cd25
Dane Bulat <dane.bulat@gmail.com>
2026-02-20 23:50:46 +0700

    refactor(controller): cleanup main accounts controller

--- packages/main/src/controller/AccountsController.ts
+++ packages/main/src/controller/AccountsController.ts
@@ -8,10 +8,6 @@ import type { ChainID } from '@polkadot-live/types/chains';
 import type { IpcTask } from '@polkadot-live/types/communication';
 
 export class AccountsController {
-  /**
-   * @name process
-   * @summary Process an address IPC task.
-   */
   static async process(task: IpcTask): Promise<string | undefined> {
     switch (task.action) {
       case 'account:getAll': {
@@ -32,10 +28,12 @@ export class AccountsController {
     }
   }
 
-  /**
-   * @name import
-   * @summary Import a new account from an address.
-   */
+  // Get all accounts as serialized.
+  private static getAll(): string {
+    return AccountsRepository.getAll();
+  }
+
+  // Import a new account.
   private static async import(task: IpcTask) {
     const { accountName }: { accountName: string } = task.data;
     NotificationsController.showNotification(
@@ -44,28 +42,14 @@ export class AccountsController {
     );
   }
 
-  /**
-   * @name remove
-   * @summary Remove a managed account.
-   */
+  // Remove an account.
   private static async remove(task: IpcTask) {
     const { address, chainId }: { address: string; chainId: ChainID } =
       task.data;
     SubscriptionsController.clearAccountTasksInStore(address, chainId);
   }
 
-  /**
-   * @name getAll
-   * @summary Send persisted accounts to frontend in serialized form.
-   */
-  private static getAll(): string {
-    return AccountsRepository.getAll();
-  }
-
-  /**
-   * @name updateAll
-   * @summary Set persisted accounts in store.
-   */
+  // Persist accounts.
   private static updateAll(task: IpcTask) {
     const { accounts }: { accounts: string } = task.data;
     AccountsRepository.replaceAll(accounts);
